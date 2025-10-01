8baa76a4daaf9e22c3c112434755fccc117a48be
Kevin Crespo <jhons.crespo@gmail.com>
2025-10-01 14:01:42 -0400

    actualizacion de falla

--- database/migrations/2025_09_29_134225_create_inscripciones_table.php
+++ database/migrations/2025_09_29_134225_create_inscripciones_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
 public function up(): void
 {
-    Schema::create('inscripcions', function (Blueprint $table) {
+    Schema::create('inscripciones', function (Blueprint $table) {
         $table->id();
         $table->foreignId('user_id')->constrained()->onDelete('cascade');
         $table->string('nombre_equipo');
